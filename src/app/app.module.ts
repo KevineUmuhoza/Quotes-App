2814f11bb664c8dcf99343eef7933281f069edfc
KevineUmuhoza <ckevny@gmail.com>
2021-02-22 00:16:13 +0200

    declared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { StrikeThroughDirective } from './directive/strikethrough.directive';
+import { TimeCountPipe } from './pipe/time-count.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    StrikeThroughDirective,
+    TimeCountPipe
+
   ],
   imports: [
     BrowserModule,
