fd36ceced4ef79f324a681cd85bf0555e6ef86b6
KevineUmuhoza <ckevny@gmail.com>
2021-02-21 23:39:08 +0200

    initial commit

---
+++ src/models/quote.class.ts
@@ -0,0 +1,9 @@
+export class Quote {
+    public quote: string;
+    public author: string;
+    public submitter: string;
+
+    constructor(quote: string, author: string, submitter: string){
+        
+    }
+}
